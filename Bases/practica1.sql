122071fabfc8c60a324de4af426e5037c2307fa9
alexsandererwin <alexsandererwin@gmail.com>
2024-02-01 12:22:21 +0100

    Bases

--- Bases/practica1.sql
+++ Bases/practica1.sql
@@ -1,27 +1,26 @@
-1. Crea una consulta para mostrar la versión de MySQL.
+/*1. Crea una consulta para mostrar la versión de MySQL.*/
     select version (), current_date;
-2. Crea una consulta para mostrar la fecha actual.
+/*2. Crea una consulta para mostrar la fecha actual.*/
     select current_date;
-3. Escribe la sentencia para ver cuáles son las bases de datos (BBDD) existentes en el servidor.
+/*3. Escribe la sentencia para ver cuáles son las bases de datos (BBDD) existentes en el servidor.*/
     show databases;
-4. Escribe la sentencia para ver en que BBDD te encuentras situado. Escribe el nombre de la
+/*4. Escribe la sentencia para ver en que BBDD te encuentras situado. Escribe el nombre de la*/
     show database ();
-BBDD que te muestra el servidor de MySQL como resultado. ¿Por qué te aparece esa
-BBDD?.
-5. Selecciona la BBDD curso.
+
+/*5. Selecciona la BBDD curso.*/
     use curso;
-6. Muestra todas las tablas de la BBDD curso.
+/*6. Muestra todas las tablas de la BBDD curso.*/
     show tables;
-7. Muestra la descripción de cada una de las tablas de la BBDD curso.
+/*7. Muestra la descripción de cada una de las tablas de la BBDD curso.*/
     desc clientes;
-8. Muestra los registros para todos los campos de la tabla empleados.
+/*8. Muestra los registros para todos los campos de la tabla empleados.*/
     select * from empleados;
-9. Muestra los datos de la tabla departamentos.
+/*9. Muestra los datos de la tabla departamentos.*/
     select * from departamentos;
-10. Muestra el apellido de los empleados.
+/*10. Muestra el apellido de los empleados.*/
     select apellido from empleados;
-11. Muestra el apellido, el oficio y el salario de los empleados.
+/*11. Muestra el apellido, el oficio y el salario de los empleados.*/
     select apellido,oficio,salario from empleados;
-12. Desconectarse o salir del servidor de MySQL.
+/*12. Desconectarse o salir del servidor de MySQL.*/
     exit;
 
