baa61516db95e06b9f9673992b6f1f9ddae349ed
alexsandererwin <alexsandererwin@gmail.com>
2024-04-26 09:53:24 +0200

    Actualizar consultas SQL en Practica1_Tema7.sql

--- Bases/Tema 7/Practica1_Tema7.sql
+++ Bases/Tema 7/Practica1_Tema7.sql
@@ -15,21 +15,44 @@ where dep_no in (select distinct dep_no
 select 
     dnombre
 from departamentos
-where dep_no in(select dep_no from empleados where comision is not null);
+where dep_no in(select dep_no from empleados where dep_no is not null);
 
 3.Visualizar el nombre de aquellos departamentos en los que al menos exista un empleado con
 comisión no nula.
 
-
+   select 
+    dnombre
+from departamentos
+where dep_no in(select dep_no from empleados where comision is not null);
 
 4. Visualizar nº y nombre de aquellos departamentos que no tienen empleados.
 
-
+select
+    dep_no,dnombre
+from departamentos
+where dep_no not in(select dep_no from empleados where dep_no is not null);
 
 5. Listar las localidades donde existan departamentos con empleados cuya comisión supere el
 10% del salario.
 
+select
+    dep_no,
+    localidad
+from departamentos
+where dep_no in(select dep_no from empleados where comision > (0.10*salario));
 
 
 6. Listar, en orden alfabético por apellidos, aquellos empleados que trabajan en el
 departamento de VENTAS.
+
+select 
+    apellido,
+    oficio
+from empleados where dep_no in(select dep_no from departamentos where dep_no = 30)
+order by apellido;
+
+select 
+    apellido,
+    oficio
+from empleados where dep_no in(select dep_no from departamentos where dnombre like 'ventas')
+order by apellido;
