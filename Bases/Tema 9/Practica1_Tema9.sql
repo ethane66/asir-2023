ca978fad1f5ebbedacead99ddd1316b1532d99ea
ethane66 <alexsandererwin@gmail.com>
2024-05-21 19:20:20 +0200

    Ejercicio 9 bases de datos

--- Bases/Tema 9/Practica1_Tema9.sql
+++ Bases/Tema 9/Practica1_Tema9.sql
@@ -1,27 +1,31 @@
 1. Borra la tabla VENDEDORES de la base de datos curso, si existe.
 
-drop table vendedores;
 drop table if exists vendedores;
 
 2. Crea una tabla llamada VENDEDORES que guarde infomación de los empleados cuyo oficio es
 vendedor. La tabla VENDEDORES debe incluir los siguientes campos de la tabla EMPLEADOS:
 EMP_NO, APELLIDO, DIRECTOR, SALARIO, COMISION y DEP_NO.
 
-create table vendedores
-     select emp_no,apellido,director,salario,comision,dep_no
-     from empleados
-     where oficio = 'vendedor';
+create table vendedores 
+select emp_no, 
+apellido, 
+director, 
+salario, 
+comision, 
+dep_no from empleados 
+where oficio = "vendedor"; 
 
 3. Muestra los datos de la tabla VENDEDORES.
 
-select * from vendedores;
+mysql> select * from vendedores;
 +--------+----------+----------+---------+----------+--------+
-| emp_no | apellido | director | salario | comision | dep_no |    
-+--------+----------+----------+---------+----------+--------+    
-|   7499 | ALONSO   |     7698 | 1400.00 |   400.00 |     30 |    
-|   7654 | MARTIN   |     7698 | 1500.00 |  1600.00 |     30 |    
-|   7844 | CALVO    |     7698 | 1800.00 |     0.00 |     30 |    
-+--------+----------+----------+---------+----------+--------+    
+| emp_no | apellido | director | salario | comision | dep_no |
++--------+----------+----------+---------+----------+--------+
+|   7499 | ALONSO   |     7698 | 1400.00 |   400.00 |     30 |
+|   7654 | MARTIN   |     7698 | 1500.00 |  1600.00 |     30 |
+|   7844 | CALVO    |     7698 | 1800.00 |     0.00 |     30 |
++--------+----------+----------+---------+----------+--------+
+
 
 4. Muestra la estructura de la tabla VENDEDORES.
 
@@ -29,12 +33,12 @@ desc vendedores;
 +----------+------------+------+-----+---------+-------+
 | Field    | Type       | Null | Key | Default | Extra |
 +----------+------------+------+-----+---------+-------+
-| emp_no   | int(4)     | NO   |     | 0       |       |
+| emp_no   | int        | NO   |     | NULL    |       |
 | apellido | varchar(8) | YES  |     | NULL    |       |
-| director | int(4)     | YES  |     | NULL    |       |
+| director | int        | YES  |     | NULL    |       |
 | salario  | float(6,2) | YES  |     | NULL    |       |
 | comision | float(6,2) | YES  |     | NULL    |       |
-| dep_no   | int(2)     | YES  |     | NULL    |       |
+| dep_no   | int        | YES  |     | NULL    |       |
 +----------+------------+------+-----+---------+-------+
 
 5. Muestra la estructura de la tabla VENDEDORES de otra forma diferente a la del ejercicio 4.
@@ -53,41 +57,54 @@ contestado que no, escribe la sentencia necesaria, para asignar como clave ajena
 VENDEDORES el campo DEP_NO que referencia al campo DEP_NO de la tabla
 DEPARTAMENTOS.
 
-alter table vendedores
-add constraint fk_vendedores_departamentos
-    foreign key vendedores (dep_no)
-    references departamentos (dep_no);
+alter table vendedores 
+add constraint fk_vendedores_departamentos 
+foreign key vendedores(dep_no) 
+references departamentos(dep_no);
 
 8. ¿Debe tener la tabla VENDEDORES definida otra restricción de clave ajena? Si has
 contestado que si, escribe la sentencia necesaria para definir esta nueva clave ajena en la
 tabla VENDEDORES.
 
-
+alter table vendedores 
+add constraint fk_vendedor_director 
+foreign key vendedores(director) 
+references empleados(director);
 
 9. Mostrar EMP_NO, APELLIDO ySALARIO de la tabla VENDEDORES, de todos aquellos
 vendedores que ganan más de 1400 euros. Ordenado descendentemente por SALARIO.
 
-
+select
+    emp_no,apellido,salario
+from vendedores
+where salario > 1400
+order by salario desc;
 
 10. Mostrar EMP_NO, APELLIDO y DEP_NO de la tabla VENDEDORES y DNOMBRE y
 LOCALIDAD de la tabla DEPARTAMENTOS, de todos los vendedores. Ordenado ascendentemente
 por DEP_NO.
 
-select v.emp_no,v.apellido,v.dep_no,d.dnombre,d.localidad
-from vendedores v, departamentos d
-order by v.dep_no;
+select
+    v.emp_no,
+    v.apellido,
+    v.dep_no,
+    d.dnombre,
+    d.localidad
+from vendedores v, departamentos d 
+where v.dep_no = d.dep_no
+order by v.dep_no asc;
 
 11. Mostrar EMP_NO, APELLIDO , DIRECTOR y apellido del DIRECTOR de la tabla
 VENDEDORES. Ordenado alfabéticamente por APELLIDO del vendedor (no el de su director).
 
+select 
+    v.emp_no, 
+    v.apellido, 
+    v.director, 
+    (select e.apellido from empleados e where v.director = e.emp_no) as director_apellido 
+    from vendedores v order by v.apellido;
 
-
-12. Cuál o cuáles de estas opciones es correcta si deseamos crear una nueva tabla sólo con los
-números y nombres de los departamentos a partir de la tabla ya creada con los mismos. Si
-alguna de las opciones es incorrecta explica por qué.
-
-
-
+12. Cuál o cuáles de estas opciones es correcta si deseamos crear una nueva tabla sólo con los números y nombres de los departamentos a partir de la tabla ya creada con los mismos. Si alguna de las opciones es incorrecta explica por qué.
 /* Opción a */
 CREATE TABLE TABLA1
  SELECT dep_no, dnombre
@@ -96,11 +113,46 @@ CREATE TABLE TABLA1
 CREATE TABLE TABLA2
  SELECT dep_no "departamento", dnombre "nombre"
  FROM departamentos;
+
+Son iguales solo que en la segunda los alias cambian los titulos de las columnas
+mysql> desc tabla1;
++---------+-------------+------+-----+---------+-------+
+| Field   | Type        | Null | Key | Default | Extra |
++---------+-------------+------+-----+---------+-------+
+| dep_no  | int(2)      | NO   |     | 0       |       |
+| dnombre | varchar(14) | YES  |     | NULL    |       |
++---------+-------------+------+-----+---------+-------+
+2 rows in set (0.00 sec)
+
+mysql> desc tabla2;
++--------------+-------------+------+-----+---------+-------+
+| Field        | Type        | Null | Key | Default | Extra |
++--------------+-------------+------+-----+---------+-------+
+| departamento | int(2)      | NO   |     | 0       |       |
+| nombre       | varchar(14) | YES  |     | NULL    |       |
++--------------+-------------+------+-----+---------+-------+
+2 rows in set (0.00 sec)
+
 13. ¿Cuál es el resultado de la ejecución de esta sentecia SELECT * FROM TABLA1; ?
-
-
+mysql> select * from tabla1;
++--------+---------------+
+| dep_no | dnombre       |
++--------+---------------+
+|     10 | CONTABILIDAD  |
+|     20 | INVESTIGACION |
+|     30 | VENTAS        |
+|     40 | PRODUCCION    |
++--------+---------------+
+4 rows in set (0.00 sec)
 
 14. ¿Cuál es el resultado de la ejecución de esta sentecia SELECT * FROM TABLA2; ?
-
-
-
+mysql> select * from tabla2;
++--------------+---------------+
+| departamento | nombre        |
++--------------+---------------+
+|           10 | CONTABILIDAD  |
+|           20 | INVESTIGACION |
+|           30 | VENTAS        |
+|           40 | PRODUCCION    |
++--------------+---------------+
+4 rows in set (0.00 sec)
--- Bases/Tema 9/Practica2_Tema9.sql
+++ Bases/Tema 9/Practica2_Tema9.sql
@@ -1,27 +1,35 @@
 1. Modifica la tabla EMPLEADOS para aumentar el sueldo en 500 euros a todos los empleados cuyo
 departamento no está en MADRID. ¿Es posible? Si has contestado que no, explica por qué.
 
-
+update empleados
+set salario = salario + 500
+where dep_no in (select dep_no from departamentos where localidad not like 'madrid');
 
 2. Modifica en la tabla EMPLEADOS el nº de departamento del empleado 7900 para que sea el de
 VENTAS. ¿Es posible? Si has contestado que no, explica por qué.
 
-
+update empleados
+set dep_no = (select dep_no from departamentos where dnombre like 'ventas') 
+where emp_no = 7900;
 
 3. Elimina de la tabla departamentos aquellos que no tienen ningún empleado. ¿Es posible? Si has
 contestado que no, explica por qué.
 
-
+delete from departamentos
+where dep_no not in (select distinct dep_no from empleados);
 
 4. Elimina a todos los empleados cuyo departamento no está en MADRID. ¿Es posible? Si has
 contestado que no, explica por qué.
 
-
+delete from empleados 
+where dep_no != (select dep_no from departamentos where localidad = "madrid");
+No se puede
 
 5. Borra la tabla VENDEDORES de la base de datos curso, si existe.
+
+drop table if exists vendedores;
+
 6. Crea una tabla llamada VENDEDORES, utilizando obligatoriamente la siguiente instrucción SQL:
-
-
 
 CREATE TABLE VENDEDORES(
 EMP_NO INT(4) PRIMARY KEY,
@@ -40,22 +48,43 @@ CONSTRAINT FK_VENDEDORES_DEPARTAMENTOS
  REFERENCES DEPARTAMENTOS(DEP_NO)
 );
 
-
-
 7. Muestra los datos de la tabla VENDEDORES. ¿Tiene algún dato la tabla? Si has contestado que no,
 explica por qué.
 
+No tiene ningun dato porque no se han añadido instrucciones de INSERT en el ejercicio anterior
 
 
 8. Muestra la estructura de la tabla VENDEDORES.
 
-
+desc vendedores;
++------------+-------------+------+-----+---------+-------+
+| Field      | Type        | Null | Key | Default | Extra |
++------------+-------------+------+-----+---------+-------+
+| EMP_NO     | int         | NO   | PRI | NULL    |       |
+| APELLIDO   | varchar(8)  | YES  |     | NULL    |       |
+| OFICIO     | varchar(10) | YES  |     | NULL    |       |
+| DIRECTOR   | int         | YES  | MUL | NULL    |       |
+| FECHA_ALTA | date        | YES  |     | NULL    |       |
+| SALARIO    | float(6,2)  | YES  |     | NULL    |       |
+| COMISION   | float(6,2)  | YES  |     | NULL    |       |
+| DEP_NO     | int         | YES  | MUL | NULL    |       |
++------------+-------------+------+-----+---------+-------+
 
 9. Muestra la estructura de la tabla VENDEDORES de otra forma diferente a la del ejercicio 8.
 
-
+create table vendedores;
 
 10. Inserta en la tabla VENDEDORES los datos que se encuentran en la tabla EMPLEADOS, de todos
 aquellos empleados cuyo oficio es VENDEDOR.
 
-
+insert into vendedores 
+select 
+emp_no, 
+apellido, 
+oficio, 
+director, 
+fecha_alta, 
+salario, 
+comision, 
+dep_no 
+from empleados where oficio = "VENDEDOR";
